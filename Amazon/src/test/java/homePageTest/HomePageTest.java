a190305884022b75007e700e78e4959973ca38fc
Showkat Hasan <mdshowkathasan83@gmail.com>
2019-06-17 19:43:25 -0400

    Framework created

---
+++ Amazon/src/test/java/homePageTest/HomePageTest.java
@@ -0,0 +1,21 @@
+package homePageTest;
+
+import org.testng.annotations.Test;
+import reporting.TestLogger;
+import searchFeather.SearchFeature;
+
+public class HomePageTest extends SearchFeature {
+
+    @Test
+    public void test1() {
+        TestLogger.log("Browser is launched");
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        searchMenu();
+        System.out.println("Test Pass");
+
+    }
+
+
+
+}
