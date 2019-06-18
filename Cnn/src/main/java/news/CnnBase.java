7238f51528717ece3f3bb28a7efa276b345bd15d
Showkat Hasan <mdshowkathasan83@gmail.com>
2019-06-17 20:14:25 -0400

    Cnn Project Added

---
+++ Cnn/src/main/java/news/CnnBase.java
@@ -0,0 +1,13 @@
+package news;
+
+import base.Base;
+
+/**
+ * Created by rrt on 4/18/2015.
+ */
+public class CnnBase extends Base {
+
+
+
+
+}
---
+++ Cnn/src/test/java/TechNewsContents.java
@@ -0,0 +1,31 @@
+import news.Pages;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by rrt on 4/26/2015.
+ */
+    public class TechNewsContents extends CnnBase {
+
+    Pages page = null;
+    String techHeadLine = "This website claims to tell you in seconds";
+    @Test
+    public void getTechNews()throws InterruptedException {
+
+        page = PageFactory.initElements(driver, Pages.class);
+
+        page.clickByElement(page.politics);
+        navigateBack();
+        page.clickByElement(page.tech);
+        String news = getTextByCss(".cd__headline-text");
+        System.out.println(news);
+        Assert.assertEquals(news, techHeadLine);
+        sleepFor(1);
+        String description = getTextByCss(".cd__description");
+        System.out.println(description);
+        sleepFor(2);
+
+    }
+
+}
